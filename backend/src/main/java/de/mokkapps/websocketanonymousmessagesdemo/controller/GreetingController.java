b6a2835c1f2a5b7b28b4d298f0c27c343f3bea7e
Michael Hoffmann <michael.hoffmann@mokkapps.de>
2020-10-07 16:34:35 +0200

    Added WebsocketConnection handlers

--- backend/src/main/java/de/mokkapps/websocketanonymousmessagesdemo/controller/GreetingController.java
+++ backend/src/main/java/de/mokkapps/websocketanonymousmessagesdemo/controller/GreetingController.java
@@ -4,6 +4,7 @@ import de.mokkapps.websocketanonymousmessagesdemo.model.Greeting;
 import de.mokkapps.websocketanonymousmessagesdemo.model.HelloMessage;
 import de.mokkapps.websocketanonymousmessagesdemo.service.GreetingService;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
@@ -23,8 +24,8 @@ public class GreetingController {
 
     @MessageMapping("/hello")
     @SendToUser("/topic/greetings")
-    public Greeting greeting(HelloMessage message, Principal principal) throws Exception {
-        log.info("Received greeting message {} from {}", message, principal.getName());
+    public Greeting greeting(HelloMessage message, @Header("simpSessionId") String sessionId, Principal principal) throws Exception {
+        log.info("Received greeting message {} from {} with sessionId {}", message, principal.getName(), sessionId);
         greetingService.addUserName(principal.getName());
         Thread.sleep(1000); // simulated delay
         return new Greeting("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
--- backend/src/main/java/de/mokkapps/websocketanonymousmessagesdemo/service/GreetingService.java
+++ backend/src/main/java/de/mokkapps/websocketanonymousmessagesdemo/service/GreetingService.java
@@ -2,20 +2,44 @@ package de.mokkapps.websocketanonymousmessagesdemo.service;
 
 import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.web.socket.messaging.SessionConnectEvent;
+import org.springframework.web.socket.messaging.SessionDisconnectEvent;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Map;
+import java.util.Objects;
 
 @Data
 @Slf4j
 @Service
 public class GreetingService {
     private final SimpMessagingTemplate simpMessagingTemplate;
+    private static final String SIMP_SESSION_ID = "simpSessionId";
     private static final String WS_MESSAGE_TRANSFER_DESTINATION = "/topic/greetings";
     private List<String> userNames = new ArrayList<>();
+
+    /**
+     * Handles WebSocket connection events
+     */
+    @EventListener(SessionConnectEvent.class)
+    public void handleWebsocketConnectListener(SessionConnectEvent event) {
+        log.info(String.format("WebSocket connection established for sessionID %s",
+                getSessionIdFromMessageHeaders(event)));
+    }
+
+    /**
+     * Handles WebSocket disconnection events
+     */
+    @EventListener(SessionDisconnectEvent.class)
+    public void handleWebsocketDisconnectListener(SessionDisconnectEvent event) {
+        log.info(String.format("WebSocket connection closed for sessionID %s",
+                getSessionIdFromMessageHeaders(event)));
+    }
 
     GreetingService(SimpMessagingTemplate simpMessagingTemplate) {
         this.simpMessagingTemplate = simpMessagingTemplate;
@@ -31,4 +55,15 @@ public class GreetingService {
     public void addUserName(String username) {
         userNames.add(username);
     }
+
+    private String getSessionIdFromMessageHeaders(SessionDisconnectEvent event) {
+        Map<String, Object> headers = event.getMessage().getHeaders();
+        return Objects.requireNonNull(headers.get(SIMP_SESSION_ID)).toString();
+    }
+
+    private String getSessionIdFromMessageHeaders(SessionConnectEvent event) {
+        Map<String, Object> headers = event.getMessage().getHeaders();
+        return Objects.requireNonNull(headers.get(SIMP_SESSION_ID)).toString();
+    }
+
 }
